4ae6fe3f9448fc52e01d62657e71f38c706ae828
Fabiano Muscardi <fabiano.junior@araujo.com.br>
2023-03-21 18:15:48 -0300

    teste

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -11,7 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+opa rsrrsrsrsrs
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
